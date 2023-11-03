562381e3426b9b0bcf8d57bcf69851158ab96523
Yasin Mohammed <mohammedyasinym16720@gmail.com>
2023-11-03 17:12:20 +0200

    commit

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,11 @@ const Content = ({ items, handlerCheck, handlerDelete, search }) => {
 
     return (
         <main>
-            {<ItemLists
+            {items.length ? (<ItemLists
                 items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
                 handlerCheck={handlerCheck}
                 handlerDelete={handlerDelete}
-            />}
+            />) : <p style={{ marginTop: "16px" }}>Your list is empty</p>}
         </main>
     )
 }
